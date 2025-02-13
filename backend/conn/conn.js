16dae9b8fa5b4c0a7211bd6d0f91da3cf71115a9
Abhishek Verma <114013340+abhishekv1000@users.noreply.github.com>
2025-02-13 15:39:33 +0530

    Update conn.js

--- backend/conn/conn.js
+++ backend/conn/conn.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const conn = async () => {
     try {
         const response = await mongoose.connect
-          (" mongodb+srv://abhishekv1000:1221@taskmanager.jple0.mongodb.net/taskmanager?retryWrites=true&w=majority&appName=taskmanager");
+          ("mongodb+srv://abhishekv1000:1221@taskmanager.jple0.mongodb.net/taskmanager?retryWrites=true&w=majority&appName=taskmanager");
         if (response) {
             console.log("Connected to DB");
         }
