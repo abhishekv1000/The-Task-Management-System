748a26560aab2a187b32351d100eab15004511a5
Abhishek Verma <114013340+abhishekv1000@users.noreply.github.com>
2025-02-13 23:00:57 +0530

    Delete local directory

--- local/Readme for local run.txt
+++
@@ -1,3 +0,0 @@
-for run locally
-in frontend folder - npm start
-in backend folder - npm start
--- local/frontend/src/apiurl.js
+++
@@ -1,5 +0,0 @@
-// src/config/apiConfig.js
-
-const API_BASE_URL = "http://localhost:1000/api"; // Base URL for all API requests
-
-export default API_BASE_URL;
