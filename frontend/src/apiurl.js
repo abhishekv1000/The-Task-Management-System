656d8b31a59610e43f7e4cf41903aa6082c009d7
Abhishek Verma <114013340+abhishekv1000@users.noreply.github.com>
2025-03-10 09:19:22 +0530

    Update apiurl.js

--- frontend/src/apiurl.js
+++ frontend/src/apiurl.js
@@ -1,5 +1,5 @@
-// src/config/apiConfig.js
 
-const API_BASE_URL = "http://localhost:1000/api"; // Base URL for all API requests
+
+const API_BASE_URL = "http://localhost:1000/api"; 
 
 export default API_BASE_URL;
