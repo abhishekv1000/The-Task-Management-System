770f16fcf8902ec002cb037d899583af14a02216
Abhishek Verma <114013340+abhishekv1000@users.noreply.github.com>
2025-03-10 09:52:31 +0530

    Update task.js

--- backend/models/task.js
+++ backend/models/task.js
@@ -3,12 +3,10 @@ const taskSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
-    unique: true,
   },
   desc: {
     type: String,
     required: true,
-    unique: true,
   },
   important: {
     type: Boolean,
