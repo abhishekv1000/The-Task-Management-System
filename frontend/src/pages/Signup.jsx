622696509167490d1722a1886089c5e6252b63ed
Abhishek Verma <114013340+abhishekv1000@users.noreply.github.com>
2025-03-10 09:07:33 +0530

    Update Signup.jsx

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -6,10 +6,10 @@ import API_BASE_URL from "../apiurl";
 const Signup = () => {
   const history = useNavigate();
   
-  // Local state to handle input data
+
   const [Data, setData] = useState({ username: "", email: "", password: "" });
-  const [error, setError] = useState(""); // For displaying errors
-  const [showAlert, setShowAlert] = useState(false); // For showing alert box
+  const [error, setError] = useState("");
+  const [showAlert, setShowAlert] = useState(false); 
 
   const change = (e) => {
     const { name, value } = e.target;
@@ -26,7 +26,7 @@ const Signup = () => {
           Data
         );
         setData({ username: "", email: "", password: "" });
-        history("/login"); // Redirect to login page after successful signup
+        history("/login");
       }
     } catch (error) {
       setError(error.response.data.message);
@@ -39,7 +39,7 @@ const Signup = () => {
     <div className="h-screen flex flex-col items-center justify-center bg-gradient-to-r from-gray-800 via-gray-900 to-black">
       <h1 className="text-4xl font-bold text-white mb-10">YOUR TASK MANAGER</h1>
 
-      {/* Error alert box */}
+     
       {showAlert && (
         <div
           className="fixed top-0 left-1/2 transform -translate-x-1/2 w-96 p-4 bg-red-600 text-white text-center font-semibold z-50 shadow-lg rounded-lg opacity-90 transition-all duration-500 ease-in-out"
@@ -55,7 +55,7 @@ const Signup = () => {
       <div className="p-10 w-96 rounded-xl bg-white shadow-2xl">
         <div className="text-3xl font-semibold text-gray-800 text-center mb-8">Sign Up</div>
         
-        {/* Input Fields */}
+      
         <input
           autoComplete="off"
           type="text"
